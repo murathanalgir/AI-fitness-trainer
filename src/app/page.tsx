d11b0c1cf727589b7388338cbe73b494a2cc3626
Murathan <mrthnalgr34@gmail.com>
2025-05-01 04:02:02 +0300

    req"

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col min-h-screen text-foreground overflow-hidden">
+      <section className="relative z-10 py-24 flex-grow">
+        <div className="container mx-auto px-4"></div>
+      </section>
+    
+    </div>
+  )
+}
+
+export default HomePage
